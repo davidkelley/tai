d44c8c08a2638e0d387dedcb6a2941e46d171890
davidkelley <david.james.kelley@gmail.com>
2018-05-10 15:18:44 +0100

    updated README

--- __tests__/__utils__/setup.js
+++ __tests__/__utils__/setup.js
@@ -4,4 +4,4 @@ process.env.AWS_REGION = 'us-east-1';
 
 process.env.SLACK_API_URL = 'https://slack.com/api';
 
-process.env.BOT_TOKEN = 'xoxb-288900965988-bxcT1Fbe0ELnjBRcyOa8zaEo';
+process.env.BOT_TOKEN = 'xxxx';
